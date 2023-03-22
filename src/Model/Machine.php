c4512a5b3d7e1561964a5b78ac02ea7eb2f3a769
Jon Cram <webignition@gmail.com>
2023-03-22 14:54:55 +0000

    Machine::ipAddresses is a collection of non-empty string

--- src/Model/Machine.php
+++ src/Model/Machine.php
@@ -7,8 +7,8 @@ namespace SmartAssert\WorkerManagerClient\Model;
 class Machine
 {
     /**
-     * @param non-empty-string $id
-     * @param string[]         $ipAddresses
+     * @param non-empty-string   $id
+     * @param non-empty-string[] $ipAddresses
      */
     public function __construct(
         public readonly string $id,
